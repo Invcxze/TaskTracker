a6484cce2e537402d8aaa9eecb00fbf05c459237
Khramenko Vladislav Vladimirovich <vladhromenko@gmail.com>
2025-06-24 10:36:46 +0300

    updates & added Docker

--- backend/config/settings/settings.py
+++ backend/config/settings/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oy^n1^6juv4*u2ar%v8sjxq#ltv-#w$gad6z*rpx@o!m!n!yed'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
+SECRET_KEY = os.getenv("SECRET_KEY", "dummy")
 
 ALLOWED_HOSTS = []
 
