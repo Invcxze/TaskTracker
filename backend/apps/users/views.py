663622c54ca51f70ae4532da49375a9c89ee8bb8
Khramenko Vladislav Vladimirovich <vladhromenko@gmail.com>
2025-06-25 16:11:45 +0300

    fix ruff error

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 
 from .serializers.user import LogSerializer, RegSerializer
 from .utils.auth import generate_token_response
